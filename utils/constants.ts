d659dece043f6ffe85e84a191c9e446ed34e9245
akhil-haridas <4khilharidas@gmail.com>
2024-01-10 20:55:09 +0530

    added some products

---
+++ utils/constants.ts
@@ -0,0 +1,12 @@
+const products = [
+    {
+        id: 0,
+        name: "Google Pixel",
+        price: 65999
+    },
+        {
+        id: 1,
+        name: "Iphone 15 pro",
+        price: 145999
+    }
+]
