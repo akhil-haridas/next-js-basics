55ebcf6355cdcddf7be8e0effc66de4522854c69
akhil-haridas <4khilharidas@gmail.com>
2024-01-10 21:04:28 +0530

    products map

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,8 +1,14 @@
+import { products } from '@/utils/constants'
 import React from 'react'
 
 function Products() {
   return (
-    <div>Products</div>
+      <div>{products.map((product) => (
+          <div key={product.id}>
+              <h1>{product.name}</h1>
+              <h2>{ product.price }</h2>
+          </div>
+      ))}</div>
   )
 }
 
