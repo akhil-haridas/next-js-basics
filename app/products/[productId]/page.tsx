d44d035425f2476ccc93afbf6f1adba69c250f07
akhil-haridas <4khilharidas@gmail.com>
2024-01-11 20:28:48 +0530

    assigned productid to a variable

--- app/products/[productId]/page.tsx
+++ app/products/[productId]/page.tsx
@@ -6,6 +6,9 @@ interface paramsType {
 
 function ProductId({ params }: paramsType) {
     console.log(params.productId)
+
+    const id: number = parseInt(params.productId)
+    
   return (
       <div>ProductId : { params.productId }</div>
   )
