d4c09b29f4794f6ff577addcf6fd6e4048c38977
akhil-haridas <4khilharidas@gmail.com>
2024-01-11 20:18:25 +0530

    imported link

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,10 +1,11 @@
 import { products } from '@/utils/constants'
+import Link from 'next/link'
 import React from 'react'
 
 function Products() {
   return (
       <div>{products.map((product) => (
-       
+       <Link
       ))}</div>
   )
 }
