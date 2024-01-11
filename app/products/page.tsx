91b1bcd65c23596b94f56dc0080a833fc6040991
akhil-haridas <4khilharidas@gmail.com>
2024-01-11 20:19:25 +0530

    wrapped under link tag

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -5,7 +5,11 @@ import React from 'react'
 function Products() {
   return (
       <div>{products.map((product) => (
-       <Link
+          <Link href={""} key={product.id}>
+             <div key={product.id}>
+              <h1 className='font-bold text-2xl'>{product.name}</h1>
+              <h2 className='font-bold text-3xl'>{ product.price }</h2>
+          </div></Link>
       ))}</div>
   )
 }
