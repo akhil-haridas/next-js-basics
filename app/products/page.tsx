4a8060eb772e30e71d0434970c972c1e9d6364dd
akhil-haridas <4khilharidas@gmail.com>
2024-01-11 20:18:03 +0530

    removed div

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -4,10 +4,7 @@ import React from 'react'
 function Products() {
   return (
       <div>{products.map((product) => (
-          <div key={product.id}>
-              <h1 className='font-bold text-2xl'>{product.name}</h1>
-              <h2 className='font-bold text-3xl'>{ product.price }</h2>
-          </div>
+       
       ))}</div>
   )
 }
