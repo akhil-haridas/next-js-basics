72cb252e798527838e4b9bbf8565b22d63b4b8cd
akhil-haridas <4khilharidas@gmail.com>
2024-01-11 20:34:01 +0530

    variable changed into const

--- app/products/[productId]/page.tsx
+++ app/products/[productId]/page.tsx
@@ -10,7 +10,7 @@ function ProductId({ params }: paramsType) {
 
     const id: number = parseInt(params.productId)
     
-    let product = products[id]
+    const product = products[id]
 
     
   return (
